66e7f6434ea65a1b8ca060cfaebaea3250035176
Trevor Elliott <trevor@geekgateway.com>
2008-08-31 22:06:23 -0700

    Add an example to interactively test an openid provider

---
+++ examples/test.hs
@@ -0,0 +1,29 @@
+
+import Network.OpenID
+import Network.SSL
+
+import Network.Socket
+import System.Environment
+
+main = withSocketsDo $ do
+  sslInit
+  [ident,check] <- getArgs
+  case normalizeIdentifier (Identifier ident) of
+    Nothing -> putStrLn "Unable to normalize identifier"
+    Just i  -> do
+      rpi <- discover makeRequest i
+      case rpi of
+        Left err    -> putStrLn $ "discover: " ++ show err
+        Right (p,i) -> do
+          putStrLn ("found provider: " ++ show p)
+          putStrLn ("found identity: " ++ show i)
+          eam <- associate emptyAssociationMap True makeRequest p
+          case eam of
+            Left err -> putStrLn $ "associate: " ++ show err
+            Right am -> do
+              let au = authenticationURI am Setup p i check Nothing
+              print au
+              line <- getLine
+              let params = parseParams line
+              print params
+              print (verifySignature am params)
